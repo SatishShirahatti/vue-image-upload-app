e3a1fe8a0a660cabcc4b68681843af5851a05485
satish shirahatti <satish.shirahatti@cognizant.com>
2021-06-09 22:54:57 +0200

    add all

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+client id:
+f1ba8778223aa20
+cleint secret:
+159da35d28c6fa56d65a0ed7374d9d330a87680b
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="ui secondary pointing menu">
+		<a class="active item">Image storage</a>
+		</div>
+</template>
+
+<script>
+export default {
+	name: "AppHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
